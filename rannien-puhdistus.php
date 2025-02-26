485b3eba28841a7c432b75d66065a54b84502335
Franquin80 <76866239+Franquin80@users.noreply.github.com>
2025-02-26 23:06:50 +0200

    rannien-puhdistus.php

---
+++ rannien-puhdistus.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Rännien puhdistus
+*/
+get_header();
+?>
+<h1>Rännien puhdistus Paikkakunta</h1>
+<p>Löydä parhaat rännien puhdistajaa paikkakunnalla!</p>
+<div class="mainos">...</div>
+<div id="palveluntarjoajat"></div>
+<?php get_footer(); ?>
